71ad66cbe1b91e1dd48a7a19c37c8a15c9dfe7b7
BishalPandey <142412265+BishalPandey1@users.noreply.github.com>
2025-08-21 22:15:23 +0545

    Update Simple Snake game in python program .py

--- Simple Snake game in python program .py
+++ Simple Snake game in python program .py
@@ -5,10 +5,5 @@ def simple_interest(p, r, t):
 p = float(input("Enter the principal amount: "))
 r = float(input("Enter the rate of interest: "))
 t = float(input("Enter the time period: "))
-
 si = simple_interest(p, r, t)
-
 print("The simple interest is:", si)
-
-
-
